70232feaf79d2556c5bef6568010b30f7afd0e80
Sunny <godfks7636@naver.com>
2023-06-22 15:58:47 +0900

    엔드포인트 권한 부여 변경방식(최종)

--- src/main/java/com/example/security/config/ProjectConfig.java
+++ src/main/java/com/example/security/config/ProjectConfig.java
@@ -2,6 +2,8 @@ package com.example.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -25,16 +27,21 @@ public class ProjectConfig {
 
         return userDetailsService;
     }
-
-    // 운영 단계에서는 사용 x.
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 
+    // 옛날에는 AuthenticationManagerBuilder를 사용해서 userDetailsService, passwordEncoder를 설정해줬어야 함.
+    // 변경된 방식은 AuthenticationManager 빈 생성 시 위에서 작성했던 두 Bean을 자동으로 설정해 준다.
+    @Bean
+    protected AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
     // 현재는 WebSecurityConfigurerAdapter 상속을 받아 오버라이딩하지 않고 직접 SecurityFilterChain을 Bean 등록 함.
     @Bean
-    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.httpBasic();
         //모든 요청에 인증 필요.
         http.authorizeRequests()
